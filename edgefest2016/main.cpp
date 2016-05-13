be7baa661dfbfd4a349ac998b180791692c35af5
physiocheck <matthieu.kern@physiocheck.eu>
2016-05-13 23:17:15 +0200

    Randomize tiles position & looping

--- edgefest2016/main.cpp
+++ edgefest2016/main.cpp
@@ -2,15 +2,63 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/System/Clock.hpp>
 #include <SFML/System/Time.hpp>
+#include <iostream>
+#include <vector>
 
-#define TILE_NUMBER 12
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define TILE_NUMBER 6
 #define TILE_OUTLINE 3
-#define TILE_SPEED 200.f
+#define TILE_SPEED 400.f
+
+class Tile {
+public:
+    Tile(sf::RenderWindow& window, float posx, float posy) :
+        window(window),
+        tile(sf::Vector2f((window.getSize().x - 2 * TILE_OUTLINE) / 4, window.getSize().y / TILE_NUMBER - TILE_OUTLINE * 4)),
+        x(posx),
+        y(posy)
+    {
+        tile.setPosition(posx, posy);
+        tile.setFillColor(sf::Color(255, 0, 0));
+        setHorizontalPosition();
+    }
+    
+    void setHorizontalPosition() {
+        xpos = rand() % 4;
+        x = TILE_OUTLINE + ((window.getSize().x - 2 * TILE_OUTLINE) / 4) * xpos;
+        tile.setPosition(x, y);
+    }
+    
+    void move(float elapsedTime) {
+        y += elapsedTime * TILE_SPEED;
+        
+        if (y > window.getSize().y) {
+            y = (TILE_OUTLINE * 1.f + (TILE_NUMBER - 1) * window.getSize().y / TILE_NUMBER) - window.getSize().y + y - window.getSize().y;
+            setHorizontalPosition();
+        }
+        
+        tile.setPosition(x, y);
+    }
+    
+    void draw() {
+        window.draw(tile);
+    }
+
+private:
+    sf::RenderWindow& window;
+    sf::RectangleShape tile;
+    float x, y;
+    uint8_t xpos;
+};
 
 int main(int, char const**) {
     sf::RenderWindow window(sf::VideoMode(300, 1080), "SFML window");
     uint32_t width = window.getSize().x;
     uint32_t height = window.getSize().y;
+    srand (time(NULL));
 
     sf::RectangleShape grid(sf::Vector2f(width - TILE_OUTLINE * 2, height / TILE_NUMBER - TILE_OUTLINE * 4));
     grid.setPosition(TILE_OUTLINE, TILE_OUTLINE);
@@ -18,11 +66,14 @@ int main(int, char const**) {
     grid.setOutlineColor(sf::Color(255, 255, 255, 255));
     grid.setOutlineThickness(TILE_OUTLINE);
     
-    sf::RectangleShape tile(sf::Vector2f((width - 2 * TILE_OUTLINE) / 4, height / TILE_NUMBER - TILE_OUTLINE * 4));
-    tile.setPosition(TILE_OUTLINE, TILE_OUTLINE * 2);
-    tile.setFillColor(sf::Color(255, 0, 0));
+    std::vector<Tile*> tiles;
+    for (int i = 0 ; i < TILE_NUMBER + 1 ; ++i) {
+        Tile* tile = new Tile(window, TILE_OUTLINE, (TILE_OUTLINE * 2.f + i * height / TILE_NUMBER) - height);
+        tiles.push_back(tile);
+    }
     
     sf::Clock clock;
+    clock.restart();
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
@@ -38,8 +89,12 @@ int main(int, char const**) {
         sf::Time elapsedTime = clock.getElapsedTime();
         clock.restart();
         
-        tile.setPosition(tile.getPosition().x, tile.getPosition().y + (elapsedTime.asSeconds() * TILE_SPEED));
-        window.draw(tile);
+        for (int i = 0 ; i < TILE_NUMBER + 1 ; ++i) {
+            Tile* tile = tiles[i];
+            
+            tile->move(elapsedTime.asSeconds());
+            tile->draw();
+        }
 
         for (int i = 0 ; i < TILE_NUMBER ; ++i) {
             grid.setPosition(TILE_OUTLINE, TILE_OUTLINE * 2 + i * height / TILE_NUMBER);
