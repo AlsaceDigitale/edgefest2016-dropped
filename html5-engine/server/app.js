1c8f0e07761ebd571db274fc648c8948e31af1bd
Morgan Touverey Quilling <mtouverey@alembic-dev.com>
2016-05-30 00:23:30 +0200

    The server now serves the viewer's static files

--- html5-engine/server/app.js
+++ html5-engine/server/app.js
@@ -1,3 +1,10 @@
-import path from 'path';
+import express from 'express';
+import { join } from 'path';
 
-console.log('hello');
+const app = express();
+
+app.use(express.static(join(__dirname, '..', 'client', 'dist')));
+
+app.listen(3030);
+
+console.log('Server listening on localhost:3030');
