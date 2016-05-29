254d191b24c5ddefb27db579d01bb46c287459bc
Morgan Touverey Quilling <mtouverey@alembic-dev.com>
2016-05-29 21:42:40 +0200

    Add jQuery + Bootstrap

--- html5-engine/webpack.config.js
+++ html5-engine/webpack.config.js
@@ -1,24 +1,47 @@
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackBrowserPlugin = require('webpack-browser-plugin');
 
 module.exports = {
-    entry: './client/app.js',
-    
+    entry: {
+        vendor: './client/vendor.js',
+        app: './client/app.js'
+    },
+
     output: {
         path: path.join(__dirname, 'client', 'dist'),
-        filename: 'bundle.js'
+        filename: '[name].bundle.js',
+        sourceMapFilename: '[name].map',
+        chunkFilename: '[id].chunk.js'
     },
     
     module: {
         loaders: [
-            { test: /\.js$/, loader: 'babel-loader', query: { presets: 'es2015' } },
-            { test: /\.scss$/, loaders: ['style', 'css', 'resolve-url', 'sass'] }
+            { test: /\.js$/, loader: 'babel', query: { presets: 'es2015' } },
+            { test: /\.scss$/, loaders: ['style', 'css', 'resolve-url', 'sass'] },
+            { test: /bootstrap\/dist\/js\/umd\//, loader: 'imports', query: { jQuery: 'jquery' } }
         ]
     },
     
     plugins: [
-        new webpack.NoErrorsPlugin()
+        new webpack.optimize.CommonsChunkPlugin({ name: ['app', 'vendor'], minChunks: Infinity }),
+        new webpack.NoErrorsPlugin(),
+        new webpack.ProvidePlugin({
+            'jQuery'       : 'jquery',
+            '$'            : 'jquery',
+            'jquery'       : 'jquery',
+            'Tether'       : 'tether',
+            'window.Tether': 'tether'
+        }),
+        new HtmlWebpackPlugin({ template: path.join(__dirname, 'client', 'index.html') }),
+        new WebpackBrowserPlugin()
     ],
+
+    resolve: {
+        root: [path.join(__dirname, 'client')],
+        extensions: ['', '.js']
+    },
 
     devServer: {
         historyApiFallback: true,
